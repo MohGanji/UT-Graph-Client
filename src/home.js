a1be6a2caddb94713da72751eaddb9e50957cd51
Arman Rostami <arman.rostami.999@gmail.com>
2018-08-08 12:27:14 +0430

    Add Headers Component

--- src/home.js
+++ src/home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Header } from './header/header';
 
 export class Home extends React.Component {
   render() {
     return (
       <div className="container">
+        <Header />
       </div>
     );
   }
