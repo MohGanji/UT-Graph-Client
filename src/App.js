8100ef7fcad8327f21ff47751926be0387515b1a
Arman Rostami <arman.rostami.999@gmail.com>
2018-08-08 11:52:23 +0430

    Add home root

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Home } from './home';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-
+          <Route path={'/'} component={Home} />
         </Switch>
       </BrowserRouter>
     )
