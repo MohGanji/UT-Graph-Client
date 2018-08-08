38118c44af5c63eb4de9a868bac4fe950a2c5474
Arman Rostami <arman.rostami.999@gmail.com>
2018-08-08 12:27:23 +0430

    Add Nav component

---
+++ src/header/nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './nav.css';
+
+export class Nav extends React.Component {
+  render() {
+    return (
+      <ul className="navbar">
+      </ul>
+    );
+  }
+}
