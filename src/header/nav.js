7dd00c353fdbdfa322ee900326dfae2d0f5a4b76
Arman Rostami <arman.rostami.999@gmail.com>
2018-08-08 19:11:38 +0430

    Design navbar

--- src/header/nav.js
+++ src/header/nav.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './nav.css';
 import logo from '../images/logo.png';
+import { SearchBar } from './searchBar/searchBar';
 
 export class Nav extends React.Component {
   render() {
     return (
       <div>
         <ul>
-          <li><a id='logoLink' href={'/'}> <img id='logoImage' src={logo} /></a></li>
+          <li><a id="logoLink" href={'/'}> <img id="logoImage" src={logo} /></a></li>
+          <li id="searchBar"><SearchBar /></li>
+          <li class="rightElement"><a href={'/login'}>Login</a></li>
+          <li id="signup" class="active"><a href={'/signup'}>Signup</a></li>
+
         </ul>
       </div>
     );
