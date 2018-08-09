a274eb8d5b76adce8d81bc8367b1fc1ed775ecc6
Arman Rostami <arman.rostami.999@gmail.com>
2018-08-09 16:55:11 +0430

    Design nav

--- src/header/nav.js
+++ src/header/nav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './nav.css';
 import logo from '../images/logo.png';
+import background from '../images/background.jpg'
 import { SearchBar } from './searchBar/searchBar';
 
 export class Nav extends React.Component {
@@ -10,8 +11,8 @@ export class Nav extends React.Component {
         <ul>
           <li><a id="logoLink" href={'/'}> <img id="logoImage" src={logo} /></a></li>
           <li id="searchBar"><SearchBar /></li>
-          <li class="rightElement"><a href={'/login'}>Login</a></li>
-          <li id="signup" class="active"><a href={'/signup'}>Signup</a></li>
+          <li class="rightElement"><a href={'/login'}>ورود</a></li>
+          <li id="signup" class="active"><a href={'/signup'}>ثبت نام</a></li>
 
         </ul>
       </div>
