39191b1acb0fedcd7a2c628d345c3e6cf3c91087
Arman Rostami <arman.rostami.999@gmail.com>
2018-08-14 12:22:20 +0430

    Add get events to home

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -4,6 +4,30 @@ import NewEvents from './NewEvents'
 import { Slider } from './Slider'
 
 export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('/api/v1/event', {
+      method: 'GET',
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (responseJson) {
+        return responseJson.data;
+      }).then(function (events) {
+        this.setState({ events: events });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
   render() {
     return (
       <div className="container">
