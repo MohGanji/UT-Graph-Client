8986b73359f08aad99f6b1a246a25aeb3ca88530
Arman Rostami <arman.rostami.999@gmail.com>
2018-08-14 15:16:50 +0430

    Add get user events fetch

--- src/Components/User/index.js
+++ src/Components/User/index.js
@@ -2,17 +2,18 @@ import React from 'react';
 import './User.css';
 import { handleErrors } from '../../Utils/handleErrors';
 import { Header } from '../../Utils/Header';
-import { BackgroundCover } from './Background_Cover'
+import { BackgroundCover } from './Background_Cover';
 import ProfilePhoto from '../../images/deafault-background.jpeg';
-import NewEvents from '../Home/NewEvents'
-// import EventContainer from '../../Utils/EventContainer';
+import NewEvents from '../Home/NewEvents';
+import EventBox from '../../Utils/EventBox';
 
 export default class User extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      info: {}
+      info: {},
+      events: []
     }
   }
 
@@ -34,10 +35,29 @@ export default class User extends React.Component {
       .catch(function (error) {
         console.log(error);
       });
+
+    fetch(`/api/v1/user/${id}/events`)
+      .then(handleErrors)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (responseJson) {
+        return responseJson.data;
+      })
+      .then(function (events) {
+        that.setState({ events: events })
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
   render() {
     let userInfo = this.state.info.info == null ? 'دانشجو' : this.state.info.info;
+    let userEvents = this.state.events.map((event) =>
+      <EventBox event={event} />
+    );
+
     return (
       <div>
         <Header />
@@ -51,8 +71,9 @@ export default class User extends React.Component {
             <p class="user_about_text"> {userInfo} </p>
           </div>
         </div>
-        <br />
-        {/* <EventContainer /> */}
+        <div class="event_container">
+          {userEvents}
+        </div>
       </div >
     );
   }
