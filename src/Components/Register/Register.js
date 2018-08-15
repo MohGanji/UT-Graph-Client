e2719cbb245fc8859f605d7593779b5403bbb069
Mojtaba Shahbazi <mojtabashahbazi1377@gmail.com>
2018-08-15 15:00:38 +0430

    zahere behtare login va signup

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -32,16 +32,18 @@ export default class Register extends React.Component {
   handleSubmit() {
     const data = this.state;
 
-    fetch('/api/v1/register', {
+    fetch('/api/v1/user/register', {
       method: "POST",
       body: JSON.stringify({ data: data })
     })
       .then(handleErrors)
       .then(function (response) {
         //TODO: link to homepage
+        console.log(response);
       })
       .catch(function (error) {
         console.log(error);
+        alert(error);
         //TODO: handle linking
       });
   }
@@ -54,10 +56,11 @@ export default class Register extends React.Component {
           <input type="text" class="login-input" placeholder="username" name="username" onChange={this.handleChange} required autofocus />
           <input type="password" class="login-input" placeholder="Password" name=" password" onChange={this.handleChange} required />
           <input type="text" class="login-input" placeholder="First name" name="firstname" onChange={this.handleChange} required />
+          <input type="text" class="login-input" placeholder="Last name" name="last" onChange={this.handleChange} required />
           <input type="email" class="login-input" placeholder="Email" name="email" onChange={this.handleChange} required />
           <input type="text" class="login-input" placeholder="Sid" name="sid" onChange={this.handleChange} required />
 
-          <input type="submit" value="Signup" class="login-button" />
+          <input type="submit" value="Signup" class="login-button" onClick={this.handleSubmit} />
           <p class="login-lost">
             <a href=""> Already have an account? Signup</a>
           </p>
