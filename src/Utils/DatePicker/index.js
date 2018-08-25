1d5344547feba9b5950cc04d706119b09ef27596
Arman Rostami <arman.rostami.999@gmail.com>
2018-08-25 22:42:45 +0430

    Create DatePicker component

---
+++ src/Utils/DatePicker/index.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import './DatePicker.css'
+import { toast } from 'react-toastify';
+
+
+export default class DatePicker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      day: '',
+      month: '',
+      year: ''
+    }
+    this.handleDayChange = this.handleDayChange.bind(this);
+    this.handleMonthChange = this.handleMonthChange.bind(this);
+    this.handleYearChange = this.handleYearChange.bind(this);
+    this.handleTime = this.handleTime.bind(this);
+  }
+
+  handleDayChange(event) {
+    const target = event.target;
+    let value = target.value;
+
+    if (value < 0 || value > 31) {
+      toast('روز باید بین ۱ تا ۳۱ باشد');
+    }
+    else if (isNaN(value)) {
+      toast('لطفا ورودی عددی وارد کنید')
+    }
+    else {
+      this.props.handleTime(value, this.state.month, this.state.year);
+      this.setState({ day: value });
+    }
+  }
+
+  handleMonthChange(event) {
+    const target = event.target;
+    let value = target.value;
+
+    if (value < 0 || value > 12) {
+      toast('ماه باید بین ۱ تا ۱۲ باشد');
+    }
+    else if (isNaN(value)) {
+      toast('لطفا ورودی عددی وارد کنید')
+    }
+    else {
+      this.props.handleTime(this.state.day, value, this.state.year);
+      this.setState({ month: value });
+    }
+  }
+
+  handleYearChange(event) {
+    const target = event.target;
+    const value = target.value;
+
+    if (value < 0 || value > 1500) {
+      toast('سال باید کوچکتر از ۱۵۰۰ باشد')
+    }
+    else if (isNaN(value)) {
+      toast('لطفا ورودی عددی وارد کنید');
+    }
+    else {
+      this.props.handleTime(this.state.day, this.state.month, value);
+      this.setState({ year: value });
+    }
+  }
+
+  handleTime() {
+    let day = this.state.day;
+    let month = this.state.month;
+    let year = this.state.year;
+
+    this.props.handleTime(day, month, year);
+  }
+
+  render() {
+    return (
+      <div class="date_picker" >
+        <input class="date_picker_day" value={this.state.day} onChange={this.handleDayChange} />
+        /
+        <input class="date_picker_month" value={this.state.month} onChange={this.handleMonthChange} />
+        /
+        <input class="date_picker_year" value={this.state.year} onChange={this.handleYearChange} />
+      </div>
+    );
+  }
+}
+
