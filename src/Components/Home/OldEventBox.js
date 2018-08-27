3813cc40e324b0fd0f763ddb8a22f215c874cf39
Arman Rostami <arman.rostami.999@gmail.com>
2018-08-27 17:00:45 +0430

    Fix linking to event bug

--- src/Components/Home/OldEventBox.js
+++ src/Components/Home/OldEventBox.js
@@ -3,17 +3,16 @@ import './OldEventBox.css';
 import EventImage from '../../images/event3.jpg'
 import MapImage from '../../images/map.svg'
 import CalenderImage from '../../images/calender.svg'
-import RoleImage from '../../images/role.svg';
 import TitleHolder from '../../Utils/TitleHolder';
 
 export default class OldEventBox extends React.Component {
 
   render() {
     return (
-      <a href={`event/${this.props.event._id}`} >
+      <a href={`/event/${this.props.event._id}`} >
         <div class="old_event_box">
           <div class="old_event_image">
-            <a href={`event/${this.props.event._id}`}> <img class="img_fill_div" src={this.props.event.poster_path == null ? EventImage : this.props.event.poster_path} /> </a >
+            <a href={`/event/${this.props.event._id}`}> <img class="img_fill_div" src={this.props.event.poster_path == null ? EventImage : this.props.event.poster_path} /> </a >
           </div>
           <div class="old_event_title">
             {this.props.event.title}
