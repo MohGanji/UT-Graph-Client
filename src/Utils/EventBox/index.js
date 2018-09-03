3f333f013fcc2f91e7f875d20900a0033c8a3baa
Arman Rostami <arman.rostami.999@gmail.com>
2018-09-04 02:06:27 +0430

    Add location and time to EventBox

--- src/Utils/EventBox/index.js
+++ src/Utils/EventBox/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import EventImage from '../../images/event3.jpg'
 import './EventBox.css'
+import getDateString from '../../Utils/getDateString';
 
 export default class EventBox extends React.Component {
 
@@ -11,17 +12,15 @@ export default class EventBox extends React.Component {
         <div class="event_img">
           <a href={`/event/${this.props.event._id}`}> <img class="img_fill_div" src={this.props.event.poster_path == null ? EventImage : this.props.event.poster_path} /> </a >
         </div>
-        <a href={`/event/${this.props.event._id}`}>  <div class="event_info">
-          <div class="event_title">
-            {this.props.event.title}
-          </div>
-          <div class="event_date">
-            {/* //TODO: add time */}
-            <span> امیرآباد </span>
-            |
-          <span> یکشنبه ۲۱ آذر </span>
-          </div>
-        </div></a>
+        <a href={`/event/${this.props.event._id}`}>
+          <div class="event_info">
+            <div class="event_title">
+              {this.props.event.title}
+            </div>
+            <div class="event_date">
+              <p>{this.props.event.location}</p> | <p>{getDateString(new Date(this.props.event.beginTime))}</p>
+            </div>
+          </div></a>
       </div>
     );
   }
