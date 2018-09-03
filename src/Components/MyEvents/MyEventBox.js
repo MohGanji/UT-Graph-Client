9ee41ec711279a7fc47d8688189badb51aea271f
Arman Rostami <arman.rostami.999@gmail.com>
2018-09-04 02:07:17 +0430

    Add link to organizer page in event page

--- src/Components/MyEvents/MyEventBox.js
+++ src/Components/MyEvents/MyEventBox.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './MyEventBox.css';
-import defaultEventImage from '../../images/userEvent.jpg';
+import defaultEventImage from '../../images/event3.jpg';
 import getDateString from '../../Utils/getDateString';
 
 export default class MyEventBox extends React.Component {
@@ -9,12 +9,12 @@ export default class MyEventBox extends React.Component {
       <div class="my_event_box_container">
         <div class="my_event_box_image_container">
           <div class="my_event_box_image">
-            <img src={defaultEventImage} />
+            <a href={`/event/${this.props.event._id}`}><img src={defaultEventImage} /></a>
           </div>
         </div>
         <div class="my_event_box_info_container">
           <div class="my_event_box_info_container_title">
-            <a href={this.props.event._id}>{this.props.event.title}</a>
+            <a href={`/event/${this.props.event._id}`}>{this.props.event.title}</a>
           </div>
           <div class="my_event_box_info_container_rest">
             <p><span>تاریخ شروع:</span> <span>{getDateString(new Date(this.props.event.beginTime))}</span></p>
