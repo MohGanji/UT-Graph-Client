9897588200f86080885d51f0cb0cda5deb8a3282
Arman Rostami <arman.rostami.999@gmail.com>
2018-09-04 02:17:19 +0430

    Delete sample events from home component

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -16,25 +16,17 @@ export default class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      events: [{ title: 1233 }, { title: 12434 }, { title: 12332123 },
-      { title: 1233 }, { title: 1233 }, { title: "1233" }
-      ],
-      old_events: [
-        { title: "همایشی که حجت سال ۹۶ برگزار کرد" },
-        { title: "همایشی که حجت سال ۹۶ برگزار کرد" },
-        { title: "همایشی که حجت سال ۹۶ برگزار کرد" },
-        { title: "همایشی که حجت سال ۹۶ برگزار کرد" },
-        { title: "همایشی که حجت سال ۹۶ برگزار کرد" }
-      ],
+      newEvents: [],
+      oldEvents: [],
       pageToken: '',
-      hasMore: true
+      hasMore: false
     }
     this.handlePaginationSubmit = this.handlePaginationSubmit.bind(this);
   }
 
   componentDidMount() {
     let that = this;
-    fetch(`/api/v1/event/`, {
+    fetch(`/api/v1/event/get/new`, {
       method: 'GET',
     })
       .then(function (response) {
@@ -42,7 +34,26 @@ export default class Home extends React.Component {
       })
       .then(handleErrors)
       .then(function (responseJson) {
-        that.setState({ events: responseJson.data, pageToken: responseJson.pageToken })
+        let hasMore = responseJson.data.length === 8;
+        that.setState({
+          newEvents: responseJson.data,
+          pageToken: responseJson.pageToken,
+          hasMore: hasMore
+        })
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    fetch(`/api/v1/event/get/old`, {
+      method: 'GET',
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(handleErrors)
+      .then(function (responseJson) {
+        that.setState({ oldEvents: responseJson.data, pageToken: responseJson.pageToken })
       })
       .catch(function (error) {
         console.log(error);
@@ -52,7 +63,7 @@ export default class Home extends React.Component {
   handlePaginationSubmit() {
     let that = this;
     let pageToken = this.state.pageToken;
-    fetch(`/api/v1/event/?pageToken="${pageToken}"`, {
+    fetch(`/api/v1/event/get/new?pageToken="${pageToken}"`, {
       method: 'GET',
     })
       .then(function (response) {
@@ -60,12 +71,12 @@ export default class Home extends React.Component {
       })
       .then(handleErrors)
       .then(function (responseJson) {
-        let previousEvents = that.state.events; //async okay?
+        let previousEvents = that.state.newEvents;
         let newEvents = responseJson.data;
         let hasMore = (newEvents.length === 8);
         let events = previousEvents.concat(newEvents);
         that.setState({
-          events: events,
+          newEvents: events,
           pageToken: responseJson.pageToken,
           hasMore: hasMore
         })
@@ -76,18 +87,14 @@ export default class Home extends React.Component {
   }
 
   render() {
-    const newEvents = this.state.events.map((event) => <EventBox event={event} />);
-    const oldEvents = this.state.events.map((old_event) => <OldEventBox event={old_event} />);
+    const newEvents = this.state.newEvents.map((event) => <EventBox event={event} />);
+    const oldEvents = this.state.oldEvents.map((old_event) => <OldEventBox event={old_event} />);
     return (
       <div>
         <Header />
         <div class="test">
-          {/* <img src={skyImage} /> */}
           <div class="centered">به UT Graph خوش آمدید!</div>
         </div>
-        {/* <div class="home_slider">
-          <MySlider events={this.state.events} />
-        </div> */}
         <div class="home_new_events_container">
           <div class="home_new_events_title">
             <p> رویداد های در حال برگزاری: </p>
@@ -97,13 +104,6 @@ export default class Home extends React.Component {
             <a class="load_more_button" onClick={this.handlePaginationSubmit}>رویداد های بیشتر</a>
           </div>
         </div>
-
-        {/* <OldEventBox /> */}
-        {/* {newEvents}
-        </div>
-        <div class="old_events">
-          {oldEvents} */}
-        {/* <OldEventBox events={newEvents} /> */}
         <div class="home_slider_old_events">
           <OldEventSlider events={oldEvents} />
         </div>
