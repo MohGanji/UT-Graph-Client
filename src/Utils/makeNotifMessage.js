ccf2d5eaf8b75076929faa27a65847803478e622
hadi <m.hadi.hojjat@gmail.com>
2018-09-04 02:25:58 +0430

    Make notification message compelet

--- src/Utils/makeNotifMessage.js
+++ src/Utils/makeNotifMessage.js
@@ -1,9 +1,38 @@
+import React from 'react';
 
 export default function makeMessage(notification) {
   switch (notification.type) {
     case ('REQUEST'):
-      notification.message = notification.applicant + " درخواست عضویت در رویداد "
-        + notification.event + " را دارد ";
+      notification.message =
+        <p>
+          {notification.applicant} درخواست عضویت در رویداد
+           <b> "{notification.event}" </b>
+          را دارد
+        </p>
+      break;
+    case ('ACCEPT'):
+      notification.message =
+        <p>
+          شما به عنوان کمک کننده به رویداد
+          <b> "{notification.event}" </b>
+          اضافه شده اید
+        </p>
+      break;
+    case ('REJECT'):
+      notification.message =
+        <p>
+          متاسفانه درخواست شما برای کمک به رویداد
+          <b> "{notification.event}" </b>
+          از طرف ادمین برگزار کننده رویداد رد شده است
+        </p>
+      break;
+    case ('INFORMATION'):
+      notification.message =
+        <p>
+          کمتر از یک روز تا رویداد
+          <b> "{notification.event}" </b>
+          مانده است
+        </p>
       break;
   }
   return notification;
