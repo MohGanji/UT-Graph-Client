bb4b3ea776c98ac68fdb09728d4dce8fe9cc9a54
hadi <m.hadi.hojjat@gmail.com>
2018-09-07 10:41:42 +0430

    Delete bottuns after work

--- src/Components/Notification/NotifCard.js
+++ src/Components/Notification/NotifCard.js
@@ -7,8 +7,16 @@ export default class NotifCard extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      hasBottun: Boolean
+    }
+
     this.acceptNotif = this.acceptNotif.bind(this);
     this.rejectNotif = this.rejectNotif.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({ hasBottun: this.props.notification.hasBottun });
   }
 
   acceptNotif() {
@@ -24,6 +32,7 @@ export default class NotifCard extends React.Component {
         console.log(error);
       });
 
+    this.setState({ hasBottun: false });
     toast("درخواست " + this.props.notification.applicant + " تایید شد");
   }
 
@@ -39,25 +48,25 @@ export default class NotifCard extends React.Component {
         console.log(error);
       });
 
+    this.setState({ hasBottun: false });
     toast("درخواست " + this.props.notification.applicant + " رد شد");
   }
 
   render() {
     let buttons;
-    if (this.props.notification.type == "REQUEST")
+    if (this.state.hasBottun)
       buttons = (
         <div class="notification_button">
           <button id="accept_button" onClick={this.acceptNotif}> تایید </button>
           <button id="reject_button" onClick={this.rejectNotif}> رد </button>
         </div>
       )
-    const selectedBoxStyle = {
-      border: "3px solid blue",
-      'box-shadow': "5px 10px 15px #888888",
-    }
+    const selectedBoxClass = (this.props.selected ? "selected_box" : "");
+    const bottunBoxClass = (this.state.hasBottun ? "bottun_box" : "");
 
     return (
-      <div id="container" class="notification_card_container" style={(this.props.selected) ? selectedBoxStyle : {}} >
+      <div id="container"
+        className={"notification_card_container " + selectedBoxClass + " " + bottunBoxClass}>
         <div class="notification_message">
           {this.props.notification.message}
         </div>
