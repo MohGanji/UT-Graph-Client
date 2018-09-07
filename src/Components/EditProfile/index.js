1ee89f477b93e369d06fe41209013dcad6a44e47
Mojtaba Shahbazi <mojtabashahbazi1377@gmail.com>
2018-09-07 18:41:48 +0430

    fix bug showing user photo in edit profile

--- src/Components/EditProfile/index.js
+++ src/Components/EditProfile/index.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import './EditProfile.css';
-import Header from '../../Utils/Header';
-import pencilImage from '../../images/pencil.svg';
-import prof_pic from '../../images/defaultProfile.jpg';
+import React from 'react'
+import './EditProfile.css'
+import Header from '../../Utils/Header'
+import pencilImage from '../../images/pencil.svg'
+import prof_pic from '../../images/defaultProfile.jpg'
 import 'font-awesome/css/font-awesome.min.css'
-import TitleHolder from '../../Utils/TitleHolder';
-import { toast } from 'react-toastify';
+import TitleHolder from '../../Utils/TitleHolder'
+import { toast } from 'react-toastify'
 import { connect } from 'react-redux'
 import handleErrors from '../../Utils/functions/handleErrors'
-import Footer from '../../Utils/Footer';
+import Footer from '../../Utils/Footer'
 
-import axios from 'axios';
-var path = require('path');
-
+import axios from 'axios'
+var path = require('path')
 
 function mapStateToProps(state) {
   return {
@@ -22,26 +21,25 @@ function mapStateToProps(state) {
 }
 
 class EditProfile extends React.Component {
-
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       file: null,
       image: prof_pic,
-      visibility: "visible",
-      new_password: "",
-      new_password_repeat: "",
-      firstName: "",
-      lastName: "",
-      email: "",
-      sid: "",
-      isEdited: false
+      visibility: 'visible',
+      new_password: '',
+      new_password_repeat: '',
+      firstName: '',
+      lastName: '',
+      email: '',
+      sid: '',
+      isEdited: false,
     }
     this.onChange = this.onChange.bind(this)
     this.fileUpload = this.fileUpload.bind(this)
-    this.password_change = this.password_change.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.password_change = this.password_change.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   componentDidMount() {
@@ -50,113 +48,116 @@ class EditProfile extends React.Component {
       lastName: this.props.user.lastName,
       email: this.props.user.email,
       sid: this.props.user.sid,
-      image: this.props.user.image
+      image: this.props.user.image,
     })
   }
 
   handleChange(event) {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
+    const target = event.target
+    const value = target.value
+    const name = target.name
 
     this.setState({
-      [name]: value
-    });
+      [name]: value,
+    })
   }
 
   handleSubmit() {
-    this.fileUpload();
-    let data = this.state;
-    let token = localStorage.getItem('token');
-    let that = this;
-    let form = new FormData();
-    form.append('file', this.state.file);
+    this.fileUpload()
+    let data = this.state
+    let token = localStorage.getItem('token')
+    let that = this
+    let form = new FormData()
+    form.append('file', this.state.file)
     fetch('/api/v1/user', {
-      method: "PUT",
+      method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
-        'authorization': token
+        authorization: token,
       },
       body: JSON.stringify({
-        data: data
-        , form: form
-      })
+        data: data,
+        form: form,
+      }),
     })
-      .then(function (response) {
-        return response;
+      .then(function(response) {
+        return response
       })
       .then(handleErrors)
-      .then(function () {
-        return fetch(`/api/v1/user/${that.props.user.username}`);
-      }).then(function (response) {
-        return response.json();
-      }).then(function (responseJson) {
-        that.props.dispatch({ type: 'SET_USER', user: responseJson.data });
-      }).then(function () {
+      .then(function() {
+        return fetch(`/api/v1/user/${that.props.user.username}`)
+      })
+      .then(function(response) {
+        return response.json()
+      })
+      .then(function(responseJson) {
+        that.props.dispatch({ type: 'SET_USER', user: responseJson.data })
+      })
+      .then(function() {
         toast.success('ویرایش پروفایل شما با موفقیت انجام شد')
-      }).then(function () {
+      })
+      .then(function() {
         that.setState({ isEdited: true })
-      }).catch(function (error) {
-        console.log(error);
-      });
-
+      })
+      .catch(function(error) {
+        console.log(error)
+      })
   }
 
   onChange(event) {
-    event.preventDefault();
-    let reader = new FileReader();
-    let file = event.target.files[0];
+    event.preventDefault()
+    let reader = new FileReader()
+    let file = event.target.files[0]
     reader.onloadend = () => {
       this.setState({
         file: file,
-        image: reader.result
-      });
+        image: reader.result,
+      })
     }
     reader.readAsDataURL(file)
   }
   async fileUpload() {
-    let token = localStorage.getItem('token');
+    let token = localStorage.getItem('token')
     // toast('upload');
-    const url = '/api/v1/user/upload';
+    const url = '/api/v1/user/upload'
     let data = await new FormData()
-    data.append('file', this.state.file, this.state.file.name);
+    data.append('file', this.state.file, this.state.file.name)
     let config = {
-      method: "post",
+      method: 'post',
       headers: {
-        'authorization': token
+        authorization: token,
       },
       params: {
         // "a": "b"
-      }
+      },
     }
-    axios.post(url, data, config)
-      .then((result) => {
+    axios
+      .post(url, data, config)
+      .then(result => {
         // console.log("res:");
-        console.log(result);
+        console.log(result)
       })
-      .catch(function (error) {
+      .catch(function(error) {
         // console.log("err");
-        console.log(error);
-      });
+        console.log(error)
+      })
   }
 
   password_change(event) {
-    this.handleChange(event);
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
-    let value2;
-    if (name == "new_password")
-      value2 = this.state.new_password_repeat
-    else
-      value2 = this.state.new_password
+    this.handleChange(event)
+    const target = event.target
+    const value = target.value
+    const name = target.name
+    let value2
+    if (name == 'new_password') value2 = this.state.new_password_repeat
+    else value2 = this.state.new_password
     if (value == value2)
       this.setState({
-        visibility: "visible"
+        visibility: 'visible',
       })
     else {
       this.setState({
-        visibility: "hidden"
+        visibility: 'hidden',
       })
     }
   }
@@ -164,85 +165,132 @@ class EditProfile extends React.Component {
   render() {
     // alert(this.state.image);
     // alert(path.join(__dirname));
-    let check_passwords_equal_class = { visibility: this.state.visibility };
+    let show_image = '/public/' + this.state.image
+    let check_passwords_equal_class = { visibility: this.state.visibility }
     return (
       <div>
         <Header />
         <div class="create_event_container1">
           <div class="create_event_title">
-            <div class="create_event_title_container" >
+            <div class="create_event_title_container">
               <TitleHolder title="ویرایش پروفایل" image={pencilImage} />
             </div>
           </div>
           <div class="create_event_container2">
             <div class="change_image">
-              <div class="create_event_input" >
+              <div class="create_event_input">
                 <p class="edit_header_font"> تصویر کاربر </p>
                 <div class="change_image_2">
                   <div class="prof_pic">
-                    <img src={this.state.image} alt={123} />
+                    <img src={show_image} alt={123} />
                   </div>
-                  <label class="change_button" for="upload-photo" > تغییر تصویر </label>
-                  <input type="file" id="upload-photo" onChange={this.onChange} />
+                  <label class="change_button" for="upload-photo">
+                    {' '}
+                    تغییر تصویر{' '}
+                  </label>
+                  <input
+                    type="file"
+                    id="upload-photo"
+                    onChange={this.onChange}
+                  />
                   {/* <div onClick={this.fileUpload}> تغییر عکس </div> */}
                 </div>
               </div>
             </div>
             <div class="two_info">
               <div class="create_event_rest">
-                <div class="create_event_input" >
+                <div class="create_event_input">
                   <p> نام : </p>
-                  <input name="firstName" class="create_event_rest_input" value={this.state.firstName} type="text" onChange={this.handleChange} />
+                  <input
+                    name="firstName"
+                    class="create_event_rest_input"
+                    value={this.state.firstName}
+                    type="text"
+                    onChange={this.handleChange}
+                  />
                 </div>
               </div>
               <div class="create_event_rest">
-                <div class="create_event_input" >
+                <div class="create_event_input">
                   <p> نام خانوادگی : </p>
-                  <input name="lastName" class="create_event_rest_input" value={this.state.lastName} type="text" onChange={this.handleChange} />
+                  <input
+                    name="lastName"
+                    class="create_event_rest_input"
+                    value={this.state.lastName}
+                    type="text"
+                    onChange={this.handleChange}
+                  />
                 </div>
               </div>
             </div>
             <div class="two_info">
               <div class="create_event_rest">
-                <div class="create_event_input" >
+                <div class="create_event_input">
                   <p> ایمیل : </p>
-                  <input name="email" class="create_event_rest_input" value={this.state.email} type="text" onChange={this.handleChange} />
+                  <input
+                    name="email"
+                    class="create_event_rest_input"
+                    value={this.state.email}
+                    type="text"
+                    onChange={this.handleChange}
+                  />
                 </div>
               </div>
               <div class="create_event_rest">
-                <div class="create_event_input" >
+                <div class="create_event_input">
                   <p> شماره دانشجویی : </p>
-                  <input name="sid" class="create_event_rest_input" value={this.state.sid} type="text" onChange={this.handleChange} />
+                  <input
+                    name="sid"
+                    class="create_event_rest_input"
+                    value={this.state.sid}
+                    type="text"
+                    onChange={this.handleChange}
+                  />
                 </div>
               </div>
             </div>
 
             <div class="two_info">
               <div class="create_event_rest">
-                <div class="create_event_input" >
+                <div class="create_event_input">
                   <p> رمزعبور جدید : </p>
-                  <input class="create_event_rest_input" type="password" name="new_password" onChange={this.password_change} ></input>
+                  <input
+                    class="create_event_rest_input"
+                    type="password"
+                    name="new_password"
+                    onChange={this.password_change}
+                  />
                 </div>
               </div>
               <div class="create_event_rest tekrar">
-                <div class="create_event_input" >
+                <div class="create_event_input">
                   <p> تکرار : </p>
-                  <input class="create_event_rest_input" type="password" name="new_password_repeat" onChange={this.password_change} ></input>
+                  <input
+                    class="create_event_rest_input"
+                    type="password"
+                    name="new_password_repeat"
+                    onChange={this.password_change}
+                  />
                 </div>
               </div>
               <div class="ok_sign">
-                <i class="fa fa-check" style={check_passwords_equal_class}  ></i>
+                <i class="fa fa-check" style={check_passwords_equal_class} />
               </div>
               <div class="create_event_submit_container edit_profile_submit">
-                <input class="event_page_signup_button" type="submit" onClick={this.handleSubmit} value='ثبت' />
+                <input
+                  class="event_page_signup_button"
+                  type="submit"
+                  onClick={this.handleSubmit}
+                  value="ثبت"
+                />
               </div>
             </div>
           </div>
         </div>
         <Footer />
-      </div >
-    );
+      </div>
+    )
   }
 }
 
-export default connect(mapStateToProps)(EditProfile);
+export default connect(mapStateToProps)(EditProfile)
