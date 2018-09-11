a8eae5c3715b48e59888acd452235064e7619512
Mojtaba Shahbazi <mojtabashahbazi1377@gmail.com>
2018-09-11 17:26:15 +0430

    fix search 404 bug

--- src/Utils/Header/Nav/SearchBar/index.js
+++ src/Utils/Header/Nav/SearchBar/index.js
@@ -1,46 +1,66 @@
 import React from 'react'
 import './style.css'
 import 'font-awesome/css/font-awesome.min.css'
-import Select from 'react-select';
-import { withRouter } from 'react-router-dom';
-import BaseForm from '../../../../Utils/BaseForm';
+import Select from 'react-select'
+import { withRouter } from 'react-router-dom'
+import BaseForm from '../../../../Utils/BaseForm'
+import { ToastContainer, toast } from 'react-toastify'
 
 class SearchBar extends BaseForm {
   constructor(props) {
     super(props)
     this.state = {
       keyword: '',
-      selectedOption: { value: 'رویداد', label: 'رویداد' }
+      selectedOption: { value: 'رویداد', label: 'رویداد' },
     }
 
     this.handleSubmit = this.handleSubmit.bind(this)
-    this.handleSelect = this.handleSelect.bind(this);
+    this.handleSelect = this.handleSelect.bind(this)
   }
 
-
   handleSelect(selectedOption) {
-    this.setState({ selectedOption: selectedOption });
+    this.setState({ selectedOption: selectedOption })
   }
 
   handleSubmit() {
-    let selectedOption = this.state.selectedOption.value == 'رویداد' ? 'event' : 'user';
-    let keyword = this.state.keyword;
-    let url = '/' + 'search' + '/' + selectedOption + '/' + keyword;
-    this.props.history.push(url);
+    if (this.state.keyword.length < 2) {
+      toast.error('طول عبارت باید حداقل دو باشد')
+      return
+    } else {
+      let selectedOption =
+        this.state.selectedOption.value == 'رویداد' ? 'event' : 'user'
+      let keyword = this.state.keyword
+      let url = '/' + 'search' + '/' + selectedOption + '/' + keyword
+      this.props.history.push(url)
+    }
   }
 
   render() {
-    let selectOptions = [{ value: 'رویداد', label: 'رویداد' },
-    { value: 'کابر', label: 'کاربر' }]
+    let selectOptions = [
+      { value: 'رویداد', label: 'رویداد' },
+      { value: 'کابر', label: 'کاربر' },
+    ]
 
     return (
-      <div class="search_bar" >
-        <form id="search_form" onSubmit={this.handleSubmit} >
-          <input class="search" type="search" placeholder="عبارت مورد نظر خود را وارد کنید..." onChange={this.handleChange} value={this.state.keyword} name="keyword" />
+      <div class="search_bar">
+        {/* <ToastContainer /> */}
+        <form id="search_form" onSubmit={this.handleSubmit}>
+          <input
+            class="search"
+            type="search"
+            placeholder="عبارت مورد نظر خود را وارد کنید..."
+            onChange={this.handleChange}
+            value={this.state.keyword}
+            name="keyword"
+          />
         </form>
-        <a class="search_icon" onClick={this.handleSubmit}> <i class="fa fa-search" aria-hidden="true"></i></a>
-        <div class="select_container" >
-          <Select className="select_custom"
+        <a class="search_icon" onClick={this.handleSubmit}>
+          {' '}
+          <i class="fa fa-search" aria-hidden="true" />
+        </a>
+        <div class="select_container">
+          <Select
+            className="select_custom"
             defaultValue={selectOptions[0]}
             options={selectOptions}
             onChange={this.handleSelect}
@@ -49,8 +69,8 @@ class SearchBar extends BaseForm {
           />
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default withRouter(SearchBar);
+export default withRouter(SearchBar)
