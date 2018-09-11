5af8112862b7cfd7fb0dec7a9daf0bcb921f2908
hadi <m.hadi.hojjat@gmail.com>
2018-09-11 10:41:42 +0430

    Convert language

---
+++ src/Utils/BaseForm/numberConverter/index.js
@@ -0,0 +1,33 @@
+let persian = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹']
+let english = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+export default class NumberConverter {
+  constructor() {
+    // this.persian = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹']
+    // this.english = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+  }
+  static toEnglish(str) {
+    let res = ''
+    for (let i = 0; i < str.length; i++) {
+      let char
+      for (let j = 0; j < 10; j++)
+        if (str[i] == english[j] || str[i] == persian[j]) char = english[j]
+      if (!char) return ''
+      res += char
+    }
+    return res
+  }
+
+  static toPersian(str) {
+    let res = ''
+    for (let i = 0; i < str.length; i++) {
+      let char
+      for (let j = 0; j < 10; j++)
+        if (str[i] == english[j] || str[i] == persian[j]) char = persian[j]
+      console.log(char)
+      if (!char) return ''
+      res += char
+    }
+    return res
+  }
+}
